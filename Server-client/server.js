8bfdb5a6025095dd47e02b0fe4cfe6b6f4a79fcb
zalad <Mohaa9>
2024-11-09 10:48:48 +0300

    commit server

--- Server-client/server.js
+++ Server-client/server.js
@@ -1,25 +1,70 @@
-const http = require('http');
+const http= require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url ,req.method);
+    console.log(req.url, req.method);
+    res.writeHead(404, 'not found', {'content-type': 'text/html'});
 
-    res.writeHead(404, 'page not found', {'content-type':'text/html'});
-    fs.readFile('../view/home.html', (err, data)=>{
+    let path = '../view/';
+    switch(req.url){
+        case '/':
+            path += 'home.html';
+            res.statusCode = 200;
+        break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+        break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('location','/about');
+            res.end();
+        break;   
+           
+        default:
+            path +='404.html';
+          
+    }
+
+    
+
+    fs.readFile(path, (err, data)=>{
         if (err) {
             console.log(err);
             res.end();
             
+            
         }
         res.end(data);
     })
-
-
     
-    
-});
 
-server.listen(3000, 'localhost' ,()=>{
-    console.log('server runing port 3000');
+})
+server.listen(3000, 'localhost', ()=>{
+    console.log('the server runing this port 3000');
     
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
